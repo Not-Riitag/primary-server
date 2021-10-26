2dc9953d92f77439e0a4ab65487597c2b166f615
Matthew <matthew.elswick@matthe815.dev>
2021-10-26 15:17:52 -0400

    utilize data-controller and the dependents

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const User = require('./src/TagEngine/Classes/User')
+const UserManager = require('./data-controller/index').UserManager
 
-const user = User.createUser({
-  id: 0,
-  username: 'bendevnull'
-})
+async function run () {
+    const admin = await UserManager.getAdminUser();
+    console.log(admin)
 
-console.log(user)
-console.log(user.toJSON())
-console.log(user.toString())
-console.log(user.toString(true))
+    const user = User.createUser(admin)
+      
+    console.log(user)
+    console.log(user.toJSON())
+    console.log(user.toString())
+    console.log(user.toString(true))
+}
+
+run()
